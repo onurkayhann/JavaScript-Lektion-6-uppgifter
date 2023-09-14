3a7e3222082232e9814224413340e50e68b3fb23
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-09-14 12:00:10 +0200

    add number, decimal, text and bool

--- logics.js
+++ logics.js
@@ -1,1 +1,23 @@
 // JavaScript Lektion 6 uppgifter
+
+// UPPGIFT #1
+
+// Heltal
+var myAge = 32;
+console.log(typeof myAge); // Shows the type
+console.log(myAge); // Shows the value of myAge
+
+// Decimal
+var myDecimal = 0.3;
+console.log(typeof myDecimal); // Shows the type
+console.log(myDecimal); // Shows the value of myDecimal
+
+// Text
+var myName = 'Onur';
+console.log(typeof myName); // Shows the type
+console.log('My name is ' + myName); // Shows my name
+
+// Boolean
+var myBool = true;
+console.log(typeof myBool); // Shows the type
+console.log('Is this true or false? ' + myBool); // Shows the boolean
